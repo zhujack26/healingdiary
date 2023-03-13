0e5e264ec9bb967a8cb13d469fc667bf01f7ca80
박주승 <juseungpark@naver.com>
2023-03-13 17:59:01 +0900

    [STYLE]:GroupPage

---
+++ FE/components/group/GroupList3.jsx
@@ -0,0 +1,72 @@
+import { Pressable } from "react-native";
+import { StatusBar, FlatList, SafeAreaView, View, StyleSheet, Image, Text } from "react-native";
+import { GlobalColors } from "../../constants/color";
+
+import Title from "../../ui/Title";
+
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
+
+const Item = ({title}) => (
+  <View style={styles.item}>
+    <Image
+      source={require("../../assets/images/SAMPLE1.png")}
+      style={styles.image}
+    />
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+
+const GroupList3 = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text >
+        추천 소모임
+      </Text>
+      <FlatList
+        data={DATA}
+        renderItem={({item}) => <Item title={item.title} />}
+        keyExtractor={item => item.id}
+      />
+      
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+    
+  },
+  item: {
+    backgroundColor: GlobalColors.colors.white500,
+    padding: 30,
+    marginVertical: 12,
+    marginHorizontal: 20,
+    borderRadius: 12,
+  },
+  image: {
+    width: 50,
+    height: 50,
+    borderRadius: 12,
+  },
+  title: {
+    fontSize: 12,
+  },
+});
+
+export default GroupList3;
+
--- FE/screens/GroupScreen.jsx
+++ FE/screens/GroupScreen.jsx
@@ -1,12 +1,74 @@
-import { View, Text, StyleSheet } from "react-native";
+import { useWindowDimensions, View, ScrollView, Text, StyleSheet } from "react-native";
+import GroupList from "../components/group/GroupList";
+import GroupList2 from "../components/group/GroupList2";
+import GroupList3 from "../components/group/GroupList3";
+import * as React from "react";
+import { TabView, TabBar, SceneMap } from "react-native-tab-view";
+import { GlobalColors } from "../constants/color";
+
 const GroupScreen = () => {
-  return (
-    <View>
-      <Text>Group</Text>
+  const FirstRoute = () => (
+    <View style={{ flex: 1, backgroundColor: GlobalColors.colors.background500 }}>
+    < GroupList/>
     </View>
+  );
+  
+  const SecondRoute = () => (
+    <View style={{ flex: 1, backgroundColor: GlobalColors.colors.background500 }}>
+    < GroupList2/>
+    </View>
+  );
+
+  const ThirdRoute = () => (
+    <View style={{ flex: 1, backgroundColor: GlobalColors.colors.background500 }}>
+    < GroupList3/>
+    </View>
+  );
+  const renderScene = SceneMap({
+    first: FirstRoute,
+    second: SecondRoute,
+    third: ThirdRoute
+  });
+  const layout = useWindowDimensions();
+
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: "first", title: "전체" },
+    { key: "second", title: "내 소모임" },
+    { key: "third", title: "추천 소모임" }
+  ]);
+
+
+
+  return (
+    <TabView
+      navigationState={{ index, routes }}
+      renderScene={renderScene}
+      onIndexChange={setIndex}
+      initialLayout={{ width: layout.width }}
+      renderTabBar={props => (
+        <TabBar
+          {...props}
+          indicatorStyle={{
+            backgroundColor: "rgb(240, 80, 20)",
+            borderRadius: 20,
+            width: 50,
+            height: 50,
+          }}
+          style={{
+            backgroundColor: GlobalColors.colors.gray400,
+            shadowColor: "transparent"
+          }}
+          pressColor={"transparent"}
+        />
+      )}
+    />
+
   );
 };
 
+const styles = StyleSheet.create({});
+
 export default GroupScreen;
 
-const styles = StyleSheet.create({});
+
