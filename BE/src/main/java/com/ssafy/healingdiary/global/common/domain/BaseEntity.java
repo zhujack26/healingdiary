d819a23c5fb9d13346e32a04eed4fa4667efebfc
김민준 <gofiction95@gmail.com>
2023-03-13 15:04:52 +0900

    [CREATE] BaseEntity created

---
+++ BE/src/main/java/com/ssafy/healingdiary/global/common/domain/BaseEntity.java
@@ -0,0 +1,35 @@
+package com.ssafy.healingdiary.global.common.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "created_date",updatable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @Column(name = "updated_date")
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+
+
+
+
+}
