539ec158d76138eb74469c210ffbe654f604988f
박주승 <juseungpark@naver.com>
2023-03-14 10:34:42 +0900

    [STYLE]:add ui groupPage

--- FE/screens/GroupScreen.jsx
+++ FE/screens/GroupScreen.jsx
@@ -1,73 +1,53 @@
-import { useWindowDimensions, View, ScrollView, Text, StyleSheet } from "react-native";
+import { View, ScrollView, Dimensions, StyleSheet } from "react-native";
 import GroupList from "../components/group/GroupList";
 import GroupList2 from "../components/group/GroupList2";
 import GroupList3 from "../components/group/GroupList3";
-import * as React from "react";
-import { TabView, TabBar, SceneMap } from "react-native-tab-view";
-import { GlobalColors } from "../constants/color";
+import { useState } from "react";
+import Buttons from "../components/main/Buttons";
 
+const deviceHeight = Dimensions.get("window").height - 130;
 const GroupScreen = () => {
-  const FirstRoute = () => (
-    <View style={{ flex: 1, backgroundColor: GlobalColors.colors.background500 }}>
-    < GroupList/>
-    </View>
-  );
-  
-  const SecondRoute = () => (
-    <View style={{ flex: 1, backgroundColor: GlobalColors.colors.background500 }}>
-    < GroupList2/>
-    </View>
-  );
-
-  const ThirdRoute = () => (
-    <View style={{ flex: 1, backgroundColor: GlobalColors.colors.background500 }}>
-    < GroupList3/>
-    </View>
-  );
-  const renderScene = SceneMap({
-    first: FirstRoute,
-    second: SecondRoute,
-    third: ThirdRoute
-  });
-  const layout = useWindowDimensions();
-
-  const [index, setIndex] = React.useState(0);
-  const [routes] = React.useState([
-    { key: "first", title: "전체" },
-    { key: "second", title: "내 소모임" },
-    { key: "third", title: "추천 소모임" }
-  ]);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const handleActiveIndex = (index) => {
+    setActiveIndex(index);
+  };
 
 
+  const renderItem = () => {
+    if (activeIndex === 0) {
+      return (
+        <ScrollView>
+          <GroupList/>
+        </ScrollView>
+      );
+    }
+    if (activeIndex === 1) {
+      return <GroupList2 />;
+    }
+    if (activeIndex === 2) 
+    return <GroupList3/>;
+  };
 
   return (
-    <TabView
-      navigationState={{ index, routes }}
-      renderScene={renderScene}
-      onIndexChange={setIndex}
-      initialLayout={{ width: layout.width }}
-      renderTabBar={props => (
-        <TabBar
-          {...props}
-          indicatorStyle={{
-            backgroundColor: "rgb(240, 80, 20)",
-            borderRadius: 20,
-            width: 50,
-            height: 50,
-          }}
-          style={{
-            backgroundColor: GlobalColors.colors.gray400,
-            shadowColor: "transparent"
-          }}
-          pressColor={"transparent"}
-        />
-      )}
-    />
-
+    <View style={styles.container}>
+      <Buttons
+        activeIndex={activeIndex}
+        handleActiveIndex={handleActiveIndex}
+      />
+      {renderItem()}
+    </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 24,
+    height: deviceHeight,
+  },
+});
+
 
 export default GroupScreen;
 
