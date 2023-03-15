95887af89e41a4d611bf87b62eeeccb19fec53d5
김혜정 <mnb3559@gmail.com>
2023-03-15 15:55:41 +0900

    [ADD] DiaryController

---
+++ BE/src/main/java/com/ssafy/healingdiary/domain/diary/controller/DiaryController.java
@@ -0,0 +1,13 @@
+package com.ssafy.healingdiary.domain.diary.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/diaries")
+public class DiaryController {
+
+}
