152284bb8d8cf462cf6dc64eea9aebba268d034e
김혜정 <mnb3559@gmail.com>
2023-03-16 09:42:43 +0900

    [ADD] DiaryService

--- BE/src/main/java/com/ssafy/healingdiary/domain/diary/service/DiaryService.java
+++ BE/src/main/java/com/ssafy/healingdiary/domain/diary/service/DiaryService.java
@@ -1,10 +1,35 @@
 package com.ssafy.healingdiary.domain.diary.service;
 
+import com.ssafy.healingdiary.domain.diary.dto.DiaryCreateRequest;
+import com.ssafy.healingdiary.domain.diary.dto.DiaryDetailResponse;
+import com.ssafy.healingdiary.domain.diary.dto.DiaryListElementResponse;
+import com.ssafy.healingdiary.domain.diary.dto.DiaryListResponse;
+import java.time.LocalDateTime;
+import java.util.LinkedList;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class DiaryService {
 
+    public DiaryListResponse getDiaryList(Long clubId, String keyword, String tag, LocalDateTime date) {
+
+        DiaryListResponse diaryListResponse = DiaryListResponse.builder().build();
+        return diaryListResponse;
+    }
+
+    public DiaryDetailResponse getDiaryDetail(Long diaryId) {
+
+        DiaryDetailResponse diaryDetailResponse = DiaryDetailResponse.builder().build();
+        return diaryDetailResponse;
+    }
+
+    public String createDiary(DiaryCreateRequest diaryCreateRequest) {
+        return "";
+    }
+
+    public void deleteDiary(Long diaryId) {
+    }
 }
