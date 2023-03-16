b5ae5fe6c780435885d8b83716e6f251c677c801
김혜정 <mnb3559@gmail.com>
2023-03-16 09:42:29 +0900

    [ADD] DiaryRepository

--- BE/src/main/java/com/ssafy/healingdiary/domain/diary/repository/DiaryRepository.java
+++ BE/src/main/java/com/ssafy/healingdiary/domain/diary/repository/DiaryRepository.java
@@ -2,7 +2,9 @@ package com.ssafy.healingdiary.domain.diary.repository;
 
 import com.ssafy.healingdiary.domain.diary.domain.Diary;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
 
 }
