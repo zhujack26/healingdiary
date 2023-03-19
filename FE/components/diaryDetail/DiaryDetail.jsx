ed0f0cc8510486f3017b7a3c9279af664bd1697f
hsjun1996 <hsjun1996@naver.com>
2023-03-19 19:12:39 +0900

    [REMOVE] : Music player

--- FE/components/diaryDetail/DiaryDetail.jsx
+++ FE/components/diaryDetail/DiaryDetail.jsx
@@ -1,6 +1,5 @@
 import { SafeAreaView, View, StyleSheet } from "react-native";
 import { GlobalColors } from "./../../constants/color";
-import MusicPlayer from "./MusicPlayer";
 
 import SoundPlayer from "./SoundPlayer";
 
@@ -9,7 +8,6 @@ const DiaryDetail = () => {
     <SafeAreaView style={styles.container}>
       <View style={styles.background}></View>
       <SoundPlayer />
-      {/* <MusicPlayer /> */}
     </SafeAreaView>
   );
 };
