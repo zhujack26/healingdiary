661ab60666fd92ba79f3c95588bc8788e644e80f
한상준 <hsjun1996@naver.com>
2023-03-20 14:41:18 +0900

    [STYLE] : Group detail header icons

--- FE/components/groupDetail/GroupDetailHeader.jsx
+++ FE/components/groupDetail/GroupDetailHeader.jsx
@@ -1,14 +1,45 @@
-import { SafeAreaView, StyleSheet, Image, Dimensions } from "react-native";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Image,
+  Dimensions,
+  View,
+  Platform,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { GlobalColors } from "./../../constants/color";
+import { useNavigation } from "@react-navigation/native";
 
 const { width } = Dimensions.get("window");
 
 const GroupDetailHeader = () => {
+  const navigation = useNavigation();
+
   return (
     <SafeAreaView style={styles.header}>
       <Image
         style={styles.image}
         source={require("../../assets/images/SAMPLE2.png")}
       />
+      <View style={styles.iconContainer}>
+        <Ionicons
+          name="chevron-back"
+          size={28}
+          color={GlobalColors.colors.secondary500}
+          onPress={() => {
+            navigation.goBack();
+          }}
+        />
+
+        <Ionicons
+          name="settings"
+          size={28}
+          color={GlobalColors.colors.secondary500}
+          onPress={() => {
+            navigation.navigate("");
+          }}
+        />
+      </View>
     </SafeAreaView>
   );
 };
@@ -27,5 +58,14 @@ const styles = StyleSheet.create({
     resizeMode: "cover",
     borderBottomRightRadius: 12,
     borderBottomLeftRadius: 12,
+  },
+  iconContainer: {
+    width: width,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 16,
+    position: "absolute",
+    top: Platform.OS === "ios" ? 56 : 32,
   },
 });
