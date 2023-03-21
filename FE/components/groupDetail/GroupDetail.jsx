25c77ceee3d3042dd8317a552c4f5705379837b8
hsjun1996 <hsjun1996@naver.com>
2023-03-22 04:01:38 +0900

    [STYLE] : Exit group

--- FE/components/groupDetail/GroupDetail.jsx
+++ FE/components/groupDetail/GroupDetail.jsx
@@ -1,16 +1,32 @@
 import { ScrollView, StyleSheet } from "react-native";
-import { useCallback, useRef } from "react";
+import { useCallback, useRef, useState } from "react";
 
 import GroupDiaryList from "./GroupDiaryList";
 import GroupIntroduction from "./GroupIntroduction";
 import GroupDetailHeader from "./GroupDetailHeader";
 import BottomModal from "./BottomModal";
+import ExitModal from "./ExitModal";
 
 const GroupDetail = () => {
   const bottomSheetModalRef = useRef(null);
+  const [exitModalVisible, setExitModalVisible] = useState(false);
+
+  const handleCloseModalPress = useCallback(() => {
+    bottomSheetModalRef.current?.close();
+  }, []);
+
   const handlePresentModalPress = useCallback(() => {
     bottomSheetModalRef.current?.present();
   }, []);
+
+  const exitCloseModalPress = () => {
+    setExitModalVisible(false);
+  };
+
+  const openExitModalAndCloseModal = () => {
+    setExitModalVisible(true);
+    handleCloseModalPress();
+  };
 
   return (
     <>
@@ -18,8 +34,18 @@ const GroupDetail = () => {
       <ScrollView style={styles.container}>
         <GroupIntroduction />
         <GroupDiaryList />
+        {exitModalVisible && (
+          <ExitModal
+            exitModalVisible={exitModalVisible}
+            exitCloseModalPress={exitCloseModalPress}
+          />
+        )}
       </ScrollView>
-      <BottomModal bottomSheetModalRef={bottomSheetModalRef} />
+      <BottomModal
+        bottomSheetModalRef={bottomSheetModalRef}
+        handleCloseModalPress={handleCloseModalPress}
+        openExitModalAndCloseModal={openExitModalAndCloseModal}
+      />
     </>
   );
 };
