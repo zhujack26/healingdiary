006b23cd44b70fb51318e36e9f009f4b7c4e339b
한상준 <hsjun1996@naver.com>
2023-03-21 15:07:04 +0900

    [STYLE] : group-update-info

--- FE/components/groupInfoUpdate/GroupInfoUpdate.jsx
+++ FE/components/groupInfoUpdate/GroupInfoUpdate.jsx
@@ -1,43 +1,13 @@
-import { View, TextInput, Text, StyleSheet } from "react-native";
-
 import GroupInfoUpdateHeader from "./GroupInfoUpdateHeader";
-import { GlobalColors } from "./../../constants/color";
+import GroupInfoUpdateIntro from "./GroupInfoUpdateIntro";
 
 const GroupInfoUpdate = () => {
   return (
     <>
       <GroupInfoUpdateHeader />
-      <View style={styles.container}>
-        <View style={styles.groupNameContainer}>
-          <Text style={styles.groupName}>소모임 이름</Text>
-          <TextInput style={styles.groupNameInput} />
-        </View>
-      </View>
+      <GroupInfoUpdateIntro />
     </>
   );
 };
 
 export default GroupInfoUpdate;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 32,
-    paddingHorizontal: 16,
-  },
-
-  groupNameContainer: {},
-
-  groupName: {
-    fontFamily: "KoddiUDOnGothic-ExtraBold",
-    fontSize: 24,
-    marginBottom: 16,
-  },
-
-  groupNameInput: {
-    fontFamily: "KoddiUDOnGothic-Regular",
-    borderBottomWidth: 2,
-    borderBottomColor: GlobalColors.colors.primary500,
-    fontSize: 16,
-  },
-});
