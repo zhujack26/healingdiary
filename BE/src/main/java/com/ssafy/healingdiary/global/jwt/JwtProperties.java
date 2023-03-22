ccfde29f7458890396d47e219b11bf9fa79ef7d2
김민준 <gofiction95@gmail.com>
2023-03-22 21:54:30 +0900

    [TEST] Between gitlab and mattermost test

--- BE/src/main/java/com/ssafy/healingdiary/global/jwt/JwtProperties.java
+++ BE/src/main/java/com/ssafy/healingdiary/global/jwt/JwtProperties.java
@@ -1,10 +1,9 @@
 package com.ssafy.healingdiary.global.jwt;
 
-//jwt properties
+//jwt properties.
 public interface JwtProperties {
     String SECRET = "cos";
     int EXPIRATION_TIME = 60000 * 10;
     String TOKEN_PREFIX = "Bearer ";
     String HEADER_STRING = "Authorization";
 }
-
