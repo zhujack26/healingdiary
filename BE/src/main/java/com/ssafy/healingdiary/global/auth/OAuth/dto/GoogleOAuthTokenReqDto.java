b5cfa087ee179f2580e08bcdcc09d659792a79c7
김민준 <gofiction95@gmail.com>
2023-03-23 21:41:01 +0900

    [UPDATE] GoogleOAuthTokenReqDto.java JsonProperty annotation added

--- BE/src/main/java/com/ssafy/healingdiary/global/auth/OAuth/dto/GoogleOAuthTokenReqDto.java
+++ BE/src/main/java/com/ssafy/healingdiary/global/auth/OAuth/dto/GoogleOAuthTokenReqDto.java
@@ -1,13 +1,17 @@
 package com.ssafy.healingdiary.global.auth.OAuth.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class GoogleOAuthTokenReqDto {
 
+    @JsonProperty("access_token")
     private String accessToken;
+    @JsonProperty("expires_in")
     private int expiresIn;
     private String scope;
+    @JsonProperty("expires_in")
     private String tokenType;
 
 
