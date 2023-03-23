d7bd2b0afc6357291df307b34c7702f0e95ccaae
한상준 <hsjun1996@naver.com>
2023-03-23 17:54:45 +0900

    [ADD] : temp-diary-detail

---
+++ FE/components/tempDiaryDetail/TempDiaryDetail.jsx
@@ -0,0 +1,85 @@
+import {
+  View,
+  SafeAreaView,
+  StyleSheet,
+  Image,
+  Dimensions,
+  Platform,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { GlobalColors } from "../../constants/color";
+
+const { width, height } = Dimensions.get("window");
+
+const TempDiaryDetail = ({ navigation }) => {
+  return (
+    <SafeAreaView style={styles.container}>
+      {/* 플레이어 창 */}
+      <View style={styles.header}>
+        <Image
+          style={styles.image}
+          source={require("../../assets/images/SAMPLE3.png")}
+        />
+        <View style={styles.circle}>
+          <Ionicons
+            name="play"
+            size={24}
+            color={GlobalColors.colors.white500}
+          />
+        </View>
+      </View>
+      <View style={styles.iconContainer}>
+        <Ionicons
+          name="chevron-back"
+          size={28}
+          color={GlobalColors.colors.secondary500}
+          onPress={() => {
+            navigation.goBack();
+          }}
+        />
+      </View>
+      {/* 댓글창 */}
+    </SafeAreaView>
+  );
+};
+
+export default TempDiaryDetail;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  header: {
+    width: width,
+    height: height / 2.5,
+  },
+
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+
+  circle: {
+    position: "absolute",
+    bottom: 15,
+    right: 15,
+    justifyContent: "center",
+    alignItems: "center",
+    width: 48,
+    height: 48,
+    borderRadius: 24,
+    borderColor: GlobalColors.colors.white500,
+    borderWidth: 3,
+  },
+
+  iconContainer: {
+    width: width,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 16,
+    position: "absolute",
+    top: Platform.OS === "ios" ? 56 : 32,
+  },
+});
