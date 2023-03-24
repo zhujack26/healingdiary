a1cf72c39a0cec2a2087ee7e8492144e6e7b31b8
ganjisriver <ganjisriver18@gmail.com>
2023-03-24 15:58:44 +0900

    [UPDATE] update jwt properties

---
+++ BE/src/main/java/com/ssafy/healingdiary/global/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.ssafy.healingdiary.global.config;
+
+import com.ssafy.healingdiary.global.jwt.JwtAuthenticationFilter;
+import com.ssafy.healingdiary.global.jwt.JwtAuthorizationFilter;
+import io.swagger.models.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+//@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+//    @Override
+//    public void configure(WebSecurity web) throws Exception {
+//        web
+//                .ignoring()
+//                .antMatchers("/swagger*/**")
+//    }
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        JwtAuthenticationFilter jwtAuthenticationFilter = new Auth
+//        http
+//            .csrf().disable()
+//            .cors()
+//                .and()
+//                .formLogin().disable()
+//                .httpBasic().disable()
+//                .authorizeRequests()
+//                .antMatchers("/auth/account/**").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                // 이후에 http메소드 등 기타 추가하면 될듯
+//            .sessionManagement()
+//            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                .addFilterBefore(jwtAuthenticationFilter, JwtAuthorizationFilter.class);
+//
+//    }
+}
