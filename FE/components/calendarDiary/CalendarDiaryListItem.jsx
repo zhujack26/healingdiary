97723acc6aed7e386f447db3aecefe9b54e2afb0
hsjun1996 <hsjun1996@naver.com>
2023-03-27 00:29:13 +0900

    [STYLE] : add calendarDiaryDetail, update ios diary shadow

--- FE/components/calendarDiary/CalendarDiaryListItem.jsx
+++ FE/components/calendarDiary/CalendarDiaryListItem.jsx
@@ -38,6 +38,13 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     borderRadius: 16,
     elevation: 5,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
     marginBottom: 8,
   },
 
