9ae3494379140434c9a8fa62e8723660073e8148
김혜정 <mnb3559@gmail.com>
2023-03-29 15:48:03 +0900

    [MERGE] Merge branches

--- BE/src/main/java/com/ssafy/healingdiary/domain/club/domain/Club.java
+++ BE/src/main/java/com/ssafy/healingdiary/domain/club/domain/Club.java
@@ -6,6 +6,7 @@ import com.ssafy.healingdiary.global.common.domain.BaseEntity;
 import com.sun.istack.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 import javax.persistence.*;
