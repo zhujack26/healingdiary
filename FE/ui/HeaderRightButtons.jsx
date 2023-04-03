3ce5456d9593b93a9a521499c68f0a96b984fc38
한상준 <hsjun1996@naver.com>
2023-04-03 17:13:21 +0900

    [ADD] : notification

--- FE/ui/HeaderRightButtons.jsx
+++ FE/ui/HeaderRightButtons.jsx
@@ -2,11 +2,13 @@ import React, { useEffect, useState, useCallback } from "react";
 import { View, TouchableOpacity, Image, StyleSheet } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { getNotification } from "../api/notification";
+import { GlobalColors } from "../constants/color";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const HeaderRightButtons = ({ navigation }) => {
   const [userImage, setUserImage] = useState(null);
   const [notice, setNotice] = useState([]);
+
   const getUserImage = useCallback(async () => {
     const data = await AsyncStorage.getItem("userImage");
     setUserImage(data);
@@ -14,8 +16,8 @@ const HeaderRightButtons = ({ navigation }) => {
 
   const getNotice = useCallback(async () => {
     const res = await getNotification();
-    setNotice(res);
-  });
+    setNotice(res ? res : []);
+  }, []);
 
   useEffect(() => {
     getUserImage();
@@ -25,6 +27,7 @@ const HeaderRightButtons = ({ navigation }) => {
   return (
     <View style={{ flexDirection: "row" }}>
       <TouchableOpacity onPress={() => navigation.navigate("Notification")}>
+        {notice.length > 0 && <View style={styles.circle}></View>}
         <MaterialCommunityIcons
           name="bell"
           size={24}
@@ -47,6 +50,16 @@ const styles = StyleSheet.create({
     height: 25,
     borderRadius: 50,
     marginRight: 30,
+  },
+
+  circle: {
+    width: 16,
+    height: 16,
+    borderRadius: 8,
+    backgroundColor: GlobalColors.colors.red500,
+    position: "absolute",
+    top: -3,
+    right: 5,
   },
 });
 
