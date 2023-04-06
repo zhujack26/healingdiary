f8dc2a90950cfe1dc609723430d74e8ea9ee312a
박주승 <juseungpark@naver.com>
2023-04-06 09:16:05 +0900

    [FIX]: delete react-native-chart-kit

--- FE/components/calendar/Statistics.jsx
+++ FE/components/calendar/Statistics.jsx
@@ -1,42 +1,90 @@
-import { View, Dimensions, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
+import {
+  VictoryBar,
+  VictoryChart,
+  VictoryAxis,
+  VictoryLabel,
+} from "victory-native";
 import { GlobalColors } from "../../constants/color";
-import { BarChart } from "react-native-chart-kit";
 
-const chartConfig = {
-  backgroundGradientFrom: GlobalColors.colors.white500,
-  backgroundGradientTo: GlobalColors.colors.white500,
-  decimalPlaces: 0, //소수점 자릿수
-  color: (opacity = 1) =>
-    GlobalColors.colors.primary500.replace("1)", `${opacity})`),
-  labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-  barPercentage: 0.5, //막대 차트의 각 막대 너비에 대한 비율 0에서 1 사이의 값을 사용
+const data = [
+  { emotion: "감동", value: 5 },
+  { emotion: "우울", value: 1 },
+  { emotion: "행복", value: 4 },
+  { emotion: "즐거움", value: 9 },
+  { emotion: "당황", value: 5 },
+];
+const getImageUriFromEmotion = (emotion) => {
+  switch (emotion) {
+    case "기쁨":
+      return "https://bje-s3-bucket.s3.ap-northeast-2.amazonaws.com/diary_default_image/%EA%B8%B0%EC%81%A8.png";
+    case "나쁨":
+      return "https://bje-s3-bucket.s3.ap-northeast-2.amazonaws.com/diary_default_image/%EB%82%98%EC%81%A8.png";
+    case "불행":
+      return "https://bje-s3-bucket.s3.ap-northeast-2.amazonaws.com/diary_default_image/%EB%B6%88%ED%96%89.png";
+    case "평온":
+      return "https://bje-s3-bucket.s3.ap-northeast-2.amazonaws.com/diary_default_image/%ED%8F%89%EC%98%A8.png";
+    case "행복":
+      return "https://bje-s3-bucket.s3.ap-northeast-2.amazonaws.com/diary_default_image/%ED%96%89%EB%B3%B5.png";
+    default:
+      return null;
+  }
 };
 
-const data = {
-  labels: ["감동", "우울", "행복", "즐거움", "당황"], //이모티콘을 못바꿈..
-  datasets: [
-    {
-      data: [5, 1, 4, 9, 5],
-    },
-  ],
-};
-
-const Statistics = () => {
+const Statistics2 = () => {
   return (
     <View style={styles.container}>
-      <BarChart
-        data={data}
+      <VictoryChart
+        domainPadding={{ x: 20 }}
+        height={200}
         width={330}
-        height={250}
-        withInnerLines={false}
-        segments={2}
-        withHorizontalLabels={false}
-        chartConfig={chartConfig}
-        verticalLabelRotation={0}
-        fromZero
-        showValuesOnTopOfBars
-        showBarTops={false}
-      />
+        padding={{ top: 10, bottom: 30, left: 50, right: 30 }}
+      >
+        <VictoryAxis
+          style={{
+            tickLabels: { fontSize: 12, padding: 15 },
+          }}
+          tickValues={data.map((d) => d.emotion)}
+        />
+        <VictoryAxis dependentAxis />
+        <VictoryBar
+          data={data}
+          x="emotion"
+          y="value"
+          barWidth={30}
+          alignment="start"
+          style={{
+            data: {
+              fill: GlobalColors.colors.primary500,
+            },
+          }}
+          labels={({ datum }) => ""}
+          labelComponent={
+            <VictoryLabel
+              dy={-20}
+              dx={-15}
+              renderInPortal={false}
+              style={{ height: 30, width: 30 }}
+              children={({ x, y, datum }) => {
+                const imageUri = getImageUriFromEmotion(datum.emotion);
+                return (
+                  <Image
+                    source={{ uri: imageUri }}
+                    style={{
+                      position: "absolute",
+                      left: x,
+                      top: y,
+                      width: 30,
+                      height: 30,
+                    }}
+                    resizeMode="contain"
+                  />
+                );
+              }}
+            />
+          }
+        />
+      </VictoryChart>
     </View>
   );
 };
@@ -51,8 +99,7 @@ const styles = StyleSheet.create({
     marginTop: 20,
     padding: 10,
     backgroundColor: GlobalColors.colors.white500,
-    //BarChart 컴포넌트는 기본적으로 왼쪽 패딩을 가지고 있음
   },
 });
 
-export default Statistics;
+export default Statistics2;
