ac8844762f8322e212593434c7593ade0c11cbbe
김민준 <gofiction95@gmail.com>
2023-04-06 09:23:51 +0900

    [FIX] Codes Trimming

--- BE/src/main/java/com/ssafy/healingdiary/domain/member/service/NoticeService.java
+++ BE/src/main/java/com/ssafy/healingdiary/domain/member/service/NoticeService.java
@@ -1,7 +1,5 @@
 package com.ssafy.healingdiary.domain.member.service;
 
-import static com.ssafy.healingdiary.global.error.ErrorCode.NOTICE_NOT_FOUND;
-
 import com.ssafy.healingdiary.domain.member.domain.CheckStatus;
 import com.ssafy.healingdiary.domain.member.domain.Member;
 import com.ssafy.healingdiary.domain.member.domain.Notice;
@@ -10,10 +8,13 @@ import com.ssafy.healingdiary.domain.member.dto.NoticeListResponse;
 import com.ssafy.healingdiary.domain.member.repository.MemberRepository;
 import com.ssafy.healingdiary.domain.member.repository.NoticeRepository;
 import com.ssafy.healingdiary.global.error.CustomException;
-import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.ssafy.healingdiary.global.error.ErrorCode.NOTICE_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
@@ -21,23 +22,24 @@ public class NoticeService {
 
     private final NoticeRepository noticeRepository;
     private final MemberRepository memberRepository;
+
     public List<NoticeListResponse> searchNoticeAll(String memberId) {
         Member member = memberRepository.getReferenceById(Long.parseLong(memberId));
-        System.out.println(memberId+"~~~~~~~~~~~~~");
+        System.out.println(memberId + "~~~~~~~~~~~~~");
         List<NoticeListResponse> list = noticeRepository.findByMember(member)
-            .stream().map((NoticeListResponse::of))
-            .collect(Collectors.toList());
+                .stream().map((NoticeListResponse::of))
+                .collect(Collectors.toList());
         return list;
     }
 
     public void changeNoticeStatus(Long noticeId) {
-        Notice notice = noticeRepository.findById(noticeId).orElseThrow(()->new CustomException(NOTICE_NOT_FOUND));
+        Notice notice = noticeRepository.findById(noticeId).orElseThrow(() -> new CustomException(NOTICE_NOT_FOUND));
         notice.changeCheckStatus(CheckStatus.CHECKED);
         noticeRepository.save(notice);
     }
 
     public DeleteNoticeRequest deleteNotice(Long noticeId) {
-        Notice notice = noticeRepository.findById(noticeId).orElseThrow(()->new CustomException(NOTICE_NOT_FOUND));
+        Notice notice = noticeRepository.findById(noticeId).orElseThrow(() -> new CustomException(NOTICE_NOT_FOUND));
         noticeRepository.delete(notice);
         return DeleteNoticeRequest.builder().noticeId(noticeId).build();
     }
